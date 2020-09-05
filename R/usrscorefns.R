606eb3c89a62ad9fbb454e73e1346a82913f660d
Xiang Ge Luo <xgluo9405@gmail.com>
2020-09-05 09:38:15 +0200

    upload

---
+++ R/usrscorefns.R
@@ -0,0 +1,34 @@
+### This function returns the objects needed to evaluate the user defined score
+usrscoreparameters <- function(initparam,
+                               usrpar = list(penType = c("AIC","BIC","other"),
+                                             L = 5,
+                                             lambda = 2,
+                                             pctesttype = "usr")){
+  
+  return(ordinalScoreParam(initparam, usrpar))
+  
+}
+
+### This function evaluates the log score of a node given its parents
+usrDAGcorescore <- function (j,parentnodes,n,param) {
+  
+  return(ordinalCoreScore(j,parentnodes,n,param))
+}
+
+### This function defines the CI tests for the starting skeleton
+usrdefinestartspace <- function(alpha,param,cpdag,n){
+
+  cormat<-cor(param$hidden_data)
+  N <- param$N
+  if(cpdag){
+    pc.skel<-pcalg::pc(suffStat = list(C = cormat, n = N),
+                       indepTest = gaussCItest,
+                       alpha=alpha,labels=colnames(param$data),skel.method="stable",verbose = FALSE)
+  } else {
+    pc.skel<-pcalg::skeleton(suffStat = list(C = cormat, n = N),
+                             indepTest = gaussCItest,
+                             alpha=alpha,labels=colnames(param$data),method="stable",verbose = FALSE)
+  }
+  
+  return(pc.skel)
+}
