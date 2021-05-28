e3b6425de0848e6521023335544485e6601c1c77
Xiang Ge Luo <luox@bs-mbpr246.d.ethz.ch>
2021-05-28 17:51:29 +0200

    update

--- R/usrscorefns_pcart.R
+++ R/usrscorefns_pcart.R
@@ -3,6 +3,7 @@ require(rpcart)
 ### This function returns the objects needed to evaluate the user defined score
 usrscoreparameters <- function(initparam, usrpar){
   
+  initparam$preLevels <- usrpar$preLevels
   initparam$pcart_alpha <- usrpar$pcart_alpha
   initparam$df <- initparam$data
   initparam$data <- apply(as.matrix(initparam$data),2,function (x) as.numeric(x))
@@ -13,9 +14,9 @@ usrscoreparameters <- function(initparam, usrpar){
 ### This function evaluates the log score of a node given its parents
 usrDAGcorescore <- function (j,parentnodes,n,param) {
   
-  res <- opt.pcart.cat(param$df, parentnodes, j, alpha = param$pcart_alpha)
+  res <- opt.pcart(param$df, parentnodes, j, param$preLevels)
   
-  return(res$score)
+  return(res$dataScore + res$structureScore)
 }
 
 
